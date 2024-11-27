c2232b1c5ab2d39fa5094dcd9abdf926082912b3
ProfSavioZoboli <savio.zoboli@edu.sc.senai.br>
2024-11-27 14:43:22 -0300

    Configuração de página de erro 404 com botão de retorno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { StyleGuideComponent } from './dev/style-guide/style-guide.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { NotFoundComponent } from './pages/error/not-found/not-found.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'auth',pathMatch:'full'},
   {path:'auth',component:AuthComponent},
-
+  {path:'**',pathMatch:'full',component:NotFoundComponent},
 
 
 
